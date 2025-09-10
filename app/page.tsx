c7b026f55d946a0e8ddd97182c133eee09d3078e
Javier Tapia <mj.tapia@hotmail.com>
2025-09-09 19:54:29 -0600

    Fix spacing in heading of HomePage component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const HomePage = () => {
   return (
     <div className="flex flex-row items-center justify-center p-8">
-      <h1 className="text-3xl font-bold ">🚀 Issue Tracker</h1>
+      <h1 className="text-3xl font-bold">🚀 Issue Tracker</h1>
     </div>
   );
 };
