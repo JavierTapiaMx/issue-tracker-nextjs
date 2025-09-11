f5d9f6776665cd2d72ec28e830c3a2447511a2f8
Javier Tapia <mj.tapia@hotmail.com>
2025-09-11 16:32:17 -0600

    Refactor LoadingIssueDetailsPage to use Skeleton components for improved loading state representation

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,5 +1,17 @@
-const LoadingIssueDetailsPage = () => {
-  return <p>Loading issue details...</p>;
+import { Skeleton } from "@/components/ui/skeleton";
+
+const LoadingIssueDetailsPage = async () => {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <Skeleton className="mb-4 h-8 w-1/4" />
+      <div className="mb-4 flex flex-row items-center gap-4">
+        <Skeleton className="h-4 w-1/12" />
+        <Skeleton className="h-4 w-1/12" />
+        <Skeleton className="h-4 w-1/12" />
+      </div>
+      <Skeleton className="h-[400px] w-full" />
+    </div>
+  );
 };
 
 export default LoadingIssueDetailsPage;
