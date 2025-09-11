3e363370e6cc1f39512f89fca1fd0e44f205f827
Javier Tapia <mj.tapia@hotmail.com>
2025-09-10 23:02:57 -0600

    Refactor form submission handler for clarity and direct integration with addIssue function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -34,17 +34,13 @@ const NewIssuePage = () => {
     }
   });
 
-  const onSubmit = async (values: AddIssueInput) => {
-    await addIssue(values);
-  };
-
   const callOutStyle =
     "rounded-md border border-red-200 bg-red-50 px-3 py-2 text-sm text-red-700";
 
   return (
     <Form {...form}>
       <form
-        onSubmit={form.handleSubmit(onSubmit)}
+        onSubmit={form.handleSubmit(async (values) => await addIssue(values))}
         className="max-w-xl space-y-3"
       >
         <FormField
