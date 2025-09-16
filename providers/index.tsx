2110565a6870062bacba104427ce277119fa2222
Javier Tapia <mj.tapia@hotmail.com>
2025-09-16 15:08:07 -0600

    Remove queryProvider from TanStack React Query because it's already implemented inside TRPC and refactor Providers component to streamline provider hierarchy

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { AuthProvider } from "./authProvider";
-import { QueryProvider } from "./queryProvider";
 import { ThemeProvider } from "./themeProvider";
 import { TrpcProvider } from "./trpcProvider";
 import { ToastProvider } from "./toastProvider";
@@ -13,22 +12,19 @@ export const Providers = ({
   children: React.ReactNode;
 } & React.ComponentProps<typeof ThemeProvider>) => {
   return (
-    <QueryProvider>
-      <AuthProvider>
-        <ThemeProvider {...themeProps}>
-          <TrpcProvider>
-            {children}
-            <ToastProvider />
-          </TrpcProvider>
-        </ThemeProvider>
-      </AuthProvider>
-    </QueryProvider>
+    <AuthProvider>
+      <ThemeProvider {...themeProps}>
+        <TrpcProvider>
+          {children}
+          <ToastProvider />
+        </TrpcProvider>
+      </ThemeProvider>
+    </AuthProvider>
   );
 };
 
 // Export individual providers for flexibility
 export { AuthProvider } from "./authProvider";
-export { QueryProvider } from "./queryProvider";
 export { ThemeProvider } from "./themeProvider";
 export { TrpcProvider } from "./trpcProvider";
 export { ToastProvider } from "./toastProvider";
