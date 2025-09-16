a75b2488839f545f00c2cf0980c6b8348cf21664
Javier Tapia <mj.tapia@hotmail.com>
2025-09-15 19:22:32 -0600

    Implement alert dialog for issue deletion confirmation; add AlertDialog component and update DeleteIssueButton

--- components/Issues/DeleteIssueButton.tsx
+++ components/Issues/DeleteIssueButton.tsx
@@ -1,4 +1,17 @@
-import Link from "next/link";
+"use client";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from "@/components/ui/alert-dialog";
+import { useIssues } from "@/hooks/useIssues";
 import { FaRegTrashAlt } from "react-icons/fa";
 import { Button } from "../ui/button";
 
@@ -7,11 +20,35 @@ interface Props {
 }
 
 const DeleteIssueButton = ({ issueId }: Props) => {
+  const { deleteIssue } = useIssues();
+
   return (
-    <Button variant="destructive">
-      <FaRegTrashAlt className="h-4 w-4" />
-      <Link href={`/issues/${issueId}/delete`}>Delete</Link>
-    </Button>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive">
+          <FaRegTrashAlt className="h-4 w-4" />
+          Delete
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Are you sure you want to delete this issue? This action cannot be
+            undone. This will permanently delete the issue.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            className="bg-destructive hover:bg-destructive/90"
+            onClick={async () => await deleteIssue(issueId)}
+          >
+            Delete Issue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
