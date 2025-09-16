139ba0df1af58badc29815d8683f24166bd2b514
Javier Tapia <mj.tapia@hotmail.com>
2025-09-16 10:44:40 -0600

    Refactor protected route logic in middleware; enable route matching for issue creation and editing

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// const isProtectedRoute = createRouteMatcher(["/issues(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/issues/new",
+  "/issues/:id/edit"
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-  // if (isProtectedRoute(req)) await auth.protect();
+  if (isProtectedRoute(req)) await auth.protect();
 });
 
 export const config = {
