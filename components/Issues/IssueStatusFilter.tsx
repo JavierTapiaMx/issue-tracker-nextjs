8307ba09975cd556eeb27ed8ed988b3b53f7bb16
Javier Tapia <mj.tapia@hotmail.com>
2025-09-17 15:30:58 -0600

    Add issue status filtering functionality to IssuesPage and create IssueStatusFilter component

---
+++ components/Issues/IssueStatusFilter.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select";
+import { IssueStatus } from "@/db/schema";
+import { useRouter } from "next/navigation";
+
+const issueStatuses: { label: string; value: IssueStatus | "all" }[] = [
+  { label: "All", value: "all" },
+  { label: "Open", value: IssueStatus.OPEN },
+  { label: "In Progress", value: IssueStatus.IN_PROGRESS },
+  { label: "Closed", value: IssueStatus.CLOSED }
+];
+
+const IssueStatusFilter = () => {
+  const router = useRouter();
+
+  const handleStatusChange = (status: string) => {
+    const query = status === "all" ? "/issues" : `/issues?status=${status}`;
+    router.push(query);
+  };
+
+  return (
+    <Select onValueChange={handleStatusChange}>
+      <SelectTrigger className="max-w-sm lg:w-full">
+        <SelectValue placeholder="Filter by Status" />
+      </SelectTrigger>
+      <SelectContent>
+        {issueStatuses.map((issueStatus) => (
+          <SelectItem key={issueStatus.value} value={issueStatus.value || ""}>
+            {issueStatus.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default IssueStatusFilter;
