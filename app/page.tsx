b7e5a508835a59264ec6e4daa8682e6bd5e23010
Javier Tapia <mj.tapia@hotmail.com>
2025-09-23 22:25:33 -0600

    Add IssuesChart component for visualizing issue status with Recharts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import IssuesSummary from "@/components/Issues/IssuesSummary";
+import IssuesChart from "@/components/Issues/IssuesChart";
 
 const HomePage = () => {
   return (
     <div className="flex flex-row items-center justify-center p-8">
-      <IssuesSummary openIssues={5} inProgressIssues={3} closedIssues={10} />
+      <IssuesChart openIssues={5} inProgressIssues={3} closedIssues={10} />
     </div>
   );
 };
