31410b1ca826d7a64f71520e0f5a411534e37819
Javier Tapia <mj.tapia@hotmail.com>
2025-09-23 22:40:41 -0600

    Refactor HomePage component to dynamically retrieve and display issue counts by status

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
 import IssuesChart from "@/components/Issues/IssuesChart";
+import IssuesSummary from "@/components/Issues/IssuesSummary";
+import LatestIssues from "@/components/Issues/LatestIssues";
+import { IssueStatus } from "@/db/schema";
+import { trpc } from "@/trpc/server";
 
-const HomePage = () => {
+const HomePage = async () => {
+  const issuesCountByStatus = await trpc.issues.getIssuesCountByStatus();
+
+  const OpenIssues =
+    issuesCountByStatus?.find((item) => item.status === IssueStatus.OPEN)
+      ?.count || 0;
+  const InProgressIssues =
+    issuesCountByStatus?.find((item) => item.status === IssueStatus.IN_PROGRESS)
+      ?.count || 0;
+  const ClosedIssues =
+    issuesCountByStatus?.find((item) => item.status === IssueStatus.CLOSED)
+      ?.count || 0;
+
   return (
-    <div className="flex flex-row items-center justify-center p-8">
-      <IssuesChart openIssues={5} inProgressIssues={3} closedIssues={10} />
+    <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+      <div className="flex flex-col gap-4">
+        <IssuesSummary
+          openIssues={OpenIssues}
+          inProgressIssues={InProgressIssues}
+          closedIssues={ClosedIssues}
+        />
+        <IssuesChart
+          openIssues={OpenIssues}
+          inProgressIssues={InProgressIssues}
+          closedIssues={ClosedIssues}
+        />
+      </div>
+      <div className="flex">
+        <LatestIssues />
+      </div>
     </div>
   );
 };
